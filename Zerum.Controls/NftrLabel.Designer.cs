0bd6b035d7f3d04c10900745be45a20701773a52
Benito Palacios <benito356@gmail.com>
2015-01-30 21:19:01 +0100

    Created control project with NftrLabel

---
+++ Zerum.Controls/NftrLabel.Designer.cs
@@ -0,0 +1,58 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="${FILENAME}.cs" company="none">
+// Copyright (C) 2015
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleonex</author>
+// <email>benito356@gmail.com</email>
+// <date>30/01/2015</date>
+//-----------------------------------------------------------------------
+namespace Zerum.Controls
+{
+    partial class NftrLabel
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // UserControl1
+            // 
+            this.Name = "NftrLabel";
+        }
+    }
+}
---
+++ Zerum.Controls/NftrLabel.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NftrLabel.cs" company="none">
+// Copyright (C) 2015
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleonex</author>
+// <email>benito356@gmail.com</email>
+// <date>30/01/2015</date>
+//-----------------------------------------------------------------------
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Nftr;
+
+namespace Zerum.Controls
+{
+    /// <summary>
+    /// Description of NftrLabel.
+    /// </summary>
+    public partial class NftrLabel : UserControl
+    {
+        readonly NftrFont font;
+        readonly int glyphHeight;
+        
+        public NftrLabel(string fontpath)
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            
+            font = new NftrFont(fontpath);
+            glyphHeight = ((Nftr.Structure.Finf)font.Blocks["Finf", 0]).GlyphHeight;
+            
+            Text = string.Empty;
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            
+            int x = 0, y = 0;
+            foreach (char ch in Text) {
+                if (ch == '\n') {
+                    x = 0;
+                    y += glyphHeight;
+                    continue;
+                }
+                
+                if (ch == '\r')
+                    continue;
+                
+                var glyph = font.SearchGlyphByChar(ch);
+                
+                if (x + glyph.Width.Width > Width) {
+                    x = 0;
+                    y += glyphHeight;
+                    continue;
+                }
+                
+                x += glyph.Width.BearingX;
+                e.Graphics.DrawImageUnscaled(glyph.ToImage(1), x, y);
+                x += glyph.Width.Advance;
+            }
+        }
+    }
+}
