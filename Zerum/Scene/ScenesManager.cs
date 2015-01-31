2cd35de9c63d09f32d330995e919b4b5afcfc891
Benito Palacios Sánchez <benito356@gmail.com>
2015-01-31 09:11:09 +0100

    Implements scenes xml

---
+++ Zerum/Scene/ScenesManager.cs
@@ -0,0 +1,74 @@
+﻿//
+//  ScenesManager.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Reflection;
+using System.IO;
+using YAXLib;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace Zerum
+{
+	public class ScenesManager
+	{
+		static readonly ScenesManager instance = new ScenesManager();
+		readonly string Filename = "Scenes.xml";
+		readonly string Filepath;
+		Dictionary<string, Scene> scenes;
+
+		private ScenesManager()
+		{
+			string execPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			Filepath = Path.Combine(execPath, Filename);
+		}
+
+		public static ScenesManager Instance {
+			get { return instance; }
+		}
+
+		private void Read()
+		{
+			var document = XDocument.Load(Filepath);
+			scenes = new Dictionary<string, Scene>();
+
+			foreach (var sceneXml in document.Root.Elements()) {
+				var sceneType = Type.GetType(sceneXml.Name.LocalName);
+				var deserializer = new YAXSerializer(sceneType);
+
+				var scene = (Scene)deserializer.Deserialize(sceneXml);
+				scenes.Add(scene.Name, scene);
+			}
+		}
+
+		private void Write()
+		{
+			var document = new XDocument();
+			var root = new XElement("ScenesManager");
+			document.Add(root);
+
+			foreach (var scene in scenes.Values) {
+				var serializer = new YAXSerializer(scene.GetType());
+				root.Add(XElement.Parse(serializer.Serialize(scene)));
+			}
+		}
+	}
+}
+
